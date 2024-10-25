e520e4843cadf91d6ac8cf86ac40a7a6c8c5b2ba
Artjpm <57558696+Artjpm@users.noreply.github.com>
2024-10-25 15:32:51 +0300

    fix an error
    error occurated on running step is fixed.
    Removed some elements that never used.

--- piano-hooks/src/App.js
+++ piano-hooks/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import "./App.css";
 import Piano from "./core/Piano";
 import mainPicture from "./img/background.png";
--- piano-hooks/src/core/Piano.js
+++ piano-hooks/src/core/Piano.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+// import React, { Fragment } from "react";
 import Instrument from "./Instrument";
 
 
